041befe914d0bd6ffc9eb88e3dd3a5e374dcad3d
J.Reczko <reczko.jakub@gmail.com>
2022-07-24 20:52:07 +0200

    Create dummy PostList component

---
+++ src/components/PostList.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react'
+
+const PostList = (): ReactElement => {
+    return (
+        <div className='post-list'>
+            App blog posts
+        </div>
+    )
+}
+
+export default PostList
