1c26c40c47f841dead4a2d24707e3543efd06998
J.Reczko <reczko.jakub@gmail.com>
2022-07-24 18:30:20 +0200

    Cleanup

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react'
+
+const App = (): ReactElement => {
+    return (
+        <div className='app ui container wide'>
+            App blog posts
+        </div>
+    )
+}
+
+export default App
